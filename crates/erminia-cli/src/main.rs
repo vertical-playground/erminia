7c2e22f50f4bff7b9eb6acf1a01d155c019c2aff
Peter Avgerinos <peteravgerinos@gmail.com>
2025-11-04 19:33:45 +0000

    feature(CLI): corrected CLI tool to not use separate pass lexer and corrected warnings in CLI

--- crates/erminia-cli/src/main.rs
+++ crates/erminia-cli/src/main.rs
@@ -1,4 +1,3 @@
-use erminia::lexer::lex::Lexer;
 use erminia::syntax::parse::Parser;
 use std::io::{self, Write};
 
@@ -15,15 +14,11 @@ fn main() -> io::Result<()> {
 
         stdin.read_line(&mut input)?;
 
-        let mut lexer = Lexer::new(&input);
-        let tokens = lexer.lex_with_separate_pass();
-
         let mut parser = Parser::new(&input);
 
         let program = parser.parse();
 
-        println!("TOKENS: {:?}", tokens);
-        println!("PROGRAM: {:?}", program.unwrap());
+        println!("{:?}", program.unwrap());
 
         // parse to AST Tree
         // check semantics
